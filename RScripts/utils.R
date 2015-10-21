67d68b4c98dbc6927f55793ad3ee0fa691ffeba2
Alex Lemm <alex23lemm@gmail.com>
2015-10-22 00:57:04 +0200

    Add xlsx.writeMultipleData

--- RScripts/utils.R
+++ RScripts/utils.R
@@ -242,3 +242,27 @@ map_name_to_acronym <- function(names, mapping) {
   
 }
 
+# Utility functions used during report generation ------------------------------
+
+
+xlsx.writeMultipleData <- function(file, sheet_name, ...)
+  # Writes several data frames to a single xlsx file
+  #
+  # Args:
+  #   file : the path to the output file
+  #    : a list of data to write to the workbook
+  
+{
+  require(xlsx, quietly = TRUE)
+  objects <- list(...)
+  fargs <- as.list(match.call(expand.dots = TRUE))
+  objnames <- as.character(fargs)[-c(1, 2)]
+  nobjects <- length(objects)
+  for (i in 1:nobjects) {
+    if (i == 1)
+      write.xlsx(objects[[i]], file, sheetName = sheet_name[i])
+    else write.xlsx(objects[[i]], file, sheetName = sheet_name[i],
+                    append = TRUE)
+  }
+}
+
